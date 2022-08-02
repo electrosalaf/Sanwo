dbf4350d0eb1f8a7d14869f539dca69f8ed5a8cf
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-02 08:37:00 +0100

    sanwo v0.1

---
+++ src/test/java/io/electrosalaf/sanwo/SanwoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.electrosalaf.sanwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
